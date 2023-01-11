bb503b8a4a09ba91260dcd932839557b0b35f6dd
leedeokmu <deolee@gmarket.com>
2023-01-11 19:42:13 +0900

    feat: add operatorTest

---
+++ src/main/java/com/freeefly/OperatorTest.java
@@ -0,0 +1,25 @@
+package com.freeefly;
+
+import com.rp.courseutil.Util;
+import java.time.Duration;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class OperatorTest {
+
+    public static void main(String[] args) {
+        Flux.range(1, 10)
+            .log()
+            .delayElements(Duration.ofSeconds(1))
+            .timeout(Duration.ofSeconds(2))
+            .subscribe(Util.subscriber())
+        ;
+        Flux.empty()
+        Util.sleepSeconds(60);
+    }
+
+    private static Mono<Integer> fallback() {
+        return Mono.fromSupplier(() -> Util.faker().random().nextInt(1, 10));
+    }
+
+}
